6b4167aeb72ad6fbfe21042b56a0ec9558109c15
Sullywan Araujo <araujo.sullywan@gamil.com>
2025-05-11 00:33:36 -0300

    feat: add query string

--- learning/API/gin-gonic/main.go
+++ learning/API/gin-gonic/main.go
@@ -20,5 +20,14 @@ func main() {
 		idade := c.Param("idade")
 		c.String(http.StatusOK, "Olá %s, voce tem %s anos", nome, idade)
 	})
+
+	//QUERY STRINGS
+	r.GET("/welcome", func(c *gin.Context) { // /welcome?firstname=Sullywan&lastname=Araujo&Guest=test
+		firstName := c.DefaultQuery("firstname", "visitante")
+		lastName := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.String(http.StatusOK, "Ola %s %s, seja bem vindo!!", firstName, lastName)
+	})
+
 	r.Run(":8080")
 }
