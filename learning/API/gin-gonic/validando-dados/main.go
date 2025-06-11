f7bd526fe74993e17172fc14d1b6587a804d31af
Sullywan Araujo <araujo.sullywan@gamil.com>
2025-06-11 02:56:32 -0300

    feat: add validando dados

---
+++ learning/API/gin-gonic/validando-dados/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type User struct {
+	Name string `json:"name" validate:"required"`
+	Age  int    `json:"idade" validate:"required,min=0,max=150"`
+	Doc  string `json:"cpf"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.POST("/welcome", func(c *gin.Context) {
+		var user User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, "Erro ao converter dados")
+			return
+		}
+
+		validate := validator.New()
+		err := validate.Struct(user)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, "Erro ao validar dados")
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+
+	})
+
+	fmt.Println("Servidor rodando na porta 8080")
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
